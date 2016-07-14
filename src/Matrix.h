834ca1b1e66cc584eeeeea582e857e1e30da43bb
joscormir <joscormir@gmail.com>
2016-07-14 15:32:27 +0200

    Implemented constructors and deconstructor

--- src/Matrix.h
+++ src/Matrix.h
@@ -1,32 +1,33 @@
 #ifndef _MATRIX_OP_
 #define _MATRIX_OP_
-template<typename T>
+
+template<typename T_>
 class Matrix {
 	
 public:
 	
-	Matrix(const T& _rows,const T& _columns); //Default fill with zeroes. 
-	Matrix(const T& _size); 
-	Matrix(const T& _mat);//Copy constructor
+	Matrix(const T_& _rows,const T_& _columns); //Default fill with zeroes. 
+	Matrix(const T_& _size); 
+	Matrix(const Matrix& _mat);//Copy constructor
 	~Matrix();
 
 	//Fill method
-	void fill(const T _values[]);
+	void fill(const T_ _values[]);
 
 	//Redefine operators to asign operations
 	Matrix& operator=	(const Matrix& _equalMat);
 
 	Matrix& operator+	(const Matrix& _addMat);
-	Matrix& operator+	(const T& _scalar);	//add scalar
+	Matrix& operator+	(const T_& _scalar);	//add scalar
 
 	Matrix& operator-	(const Matrix& _Mat);
-	Matrix& operator-	(const T& _scalar);	//subtract by scalar
+	Matrix& operator-	(const T_& _scalar);	//subtract by scalar
 
 	Matrix& operator*	(const Matrix& _secondMat);
-	Matrix& operator*	(const T& _scalar);	//Multiply by scalar
+	Matrix& operator*	(const T_& _scalar);	//Multiply by scalar
 	
 	Matrix& operator/	(const Matrix& _divideMat);
-	Matrix& operator/	(const T& _scalar);	//Divide by scalar
+	Matrix& operator/	(const T_& _scalar);	//Divide by scalar
 
 	//Particular Matrix operations
 	Matrix	transp		(const Matrix& _matToTransp);
@@ -34,17 +35,71 @@ public:
 	int		range		(const Matrix&);
 
 	//Particular matrix
-	Matrix ones(const T& _size);
-	Matrix ones(const T& _rows, const T& _columns);
+	Matrix ones(const T_& _size);
+	Matrix ones(const T_& _rows, const T_& _columns);
 
-	Matrix zero(const T& _rows, const T& _columns);
-	Matrix zero(const T& _size);
+	Matrix zero(const T_& _rows, const T_& _columns);
+	Matrix zero(const T_& _size);
 
-	Matrix identity(const T& _size);
+	Matrix identity(const T_& _size);
 	
 private:
-		T rows, columns, size;
-		T **ptrMat = nullptr;
-
+	T_ rows, columns, size;
+	T_ **ptrMat = nullptr;
 };
+
+//--------------------------------------------------------------------
+//Methods implementation
+//--------------------------------------------------------------------
+template<typename T_>
+Matrix<T_>::Matrix(const T_& _rows, const T_& _columns) : rows(_rows), columns(_columns) {
+	ptrMat = new T_*[rows];
+	for (auto i = 0; i < rows; ++i) {
+		ptrMat[i] = new T_[columns];
+	}
+	for (auto i = 0; i < rows; ++i) {
+		for (auto j = 0; j < columns; ++j) {
+			ptrMat[i][j] = 0; //initialized by default with 0s
+		}
+	}
+}
+
+//--------------------------------------------------------------------
+template<typename T_>
+Matrix<T_>::Matrix(const T_& _size) {
+	rows = _size;
+	columns = _size;
+	ptrMat = new T_*[rows];
+	for (auto i = 0; i < rows; ++i) {
+		ptrMat[i] = new T_[columns];
+	}
+}
+
+//--------------------------------------------------------------------
+template<typename T_>
+Matrix<T_>::~Matrix() {
+	for (auto i = 0; i < rows; ++i) {
+		delete[] ptrMat[i];
+	}
+	delete[] ptrMat;
+}
+
+//---------------------------------------------------------------------
+template<typename T_>
+Matrix<T_>::Matrix(const Matrix& _mat) {
+	//copy constructor
+	ptrMat = new T_*[_mat.rows];
+	for (auto i = 0; i < _mat.rows; ++i) {
+		ptrMat[i] = new T_[_mat.columns];
+	}
+	for (auto i = 0; i < _mat.rows; ++i) {
+		for (auto j = 0; j < _mat.columns; ++j) {
+			ptrMat[i][j] = _mat.ptrMat[i][j];
+		}
+	}
+	rows	= _mat.rows;
+	columns = _mat.columns;
+}
+
+
 #endif //_MATRIX_OP_
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Matrix.h"
+
+int main() {
+	
+	Matrix<int> mat(3,3);
+
+}
